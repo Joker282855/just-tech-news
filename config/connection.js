e3190c85e1562511e1e7c8d48ace93864299f553
unknown <jtjackson325@gmail.com>
2021-12-23 16:02:32 -0700

    got the typo errors fixed with the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
 // import the Seqeulize package form node .js
 const Sequelize = require('sequelize');
 
+// importing package that protects password and account information
+require('dotenv').config();
+
 // make the actual connectoin to the database
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
