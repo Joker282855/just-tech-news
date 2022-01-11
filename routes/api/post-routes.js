9cde6799f7045a95421c2e124fe7f7b6f7f0a90b
unknown <jtjackson325@gmail.com>
2022-01-10 18:27:39 -0700

    added the ability to vote on past articles

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,3 +1,4 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
 const { Post, User, Vote } = require('../../models');
 
@@ -65,9 +66,27 @@ router.put('/upvote', (req, res) => {
     Vote.create({
         user_id: req.body.user_id,
         post_id: req.body.post_id
-    })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => res.json(err));
+    }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: req.body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'created_at',
+                    [
+                        seqeulize.literal('(SELECT COUNT(*) FROM vote WHERE post_id = vote.post_id)'),
+                        'vote count'
+                    ]
+                ]
+            })
+                .then(dbPostData => res.json(dbPostData))
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        })
 })
 
 // route used to update an existing post
