cb0f1b3140029fe45f38cd68612d3d3988b63116
unknown <jtjackson325@gmail.com>
2022-01-15 17:39:52 -0700

    added the logic need to have the database deployed on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-// import the Seqeulize package form node .js
 const Sequelize = require('sequelize');
 
-// importing package that protects password and account information
 require('dotenv').config();
 
-// make the actual connectoin to the database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
+}
 
 module.exports = sequelize;
 
