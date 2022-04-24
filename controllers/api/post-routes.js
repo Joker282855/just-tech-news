33df459e58eb5fdb95ea908a9155646ff39a8b69
unknown <jtjackson325@gmail.com>
2022-04-24 10:58:35 -0600

    got the upvote button working for the website

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -84,9 +84,9 @@ router.post('/', (req, res) => {
 // route used to add the users vote to the post
 router.put('/upvote', (req, res) => {
     if (req.session) {
-        Post.upvote({...req.body, user_id: req.session.user_id}, {Vote, Comment, User })
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
 
-            .then(updatedVoteData => res.json(updatedVotedData))
+            .then(updatedVoteData => res.json(updatedVoteData))
 
             .catch(err => {
 
