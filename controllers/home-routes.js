95ffdf46c40ac8e0dd45b68c6985870facdfa0fc
unknown <jtjackson325@gmail.com>
2022-04-30 17:11:15 -0600

    got the comment form to render only when users are logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -96,7 +96,11 @@ router.get('/post/:id', (req, res) => {
 
         const post = dbPostData.get({ plain: true });
 
-        res.render('single-post', { post });
+        res.render('single-post', { 
+            post,
+
+            loggedIn: req.session.loggedIn
+         });
     })
     .catch(err => {
         console.log(err);
