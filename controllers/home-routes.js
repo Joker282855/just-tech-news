3c0297d580591f53e224fd70cae5a7cbe218ce6c
unknown <jtjackson325@gmail.com>
2022-04-30 17:41:16 -0600

    got the login and logout button to condiitionally render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,7 +36,11 @@ router.get('/', (req, res) => {
 
             const posts = dbPostData.map(post => post.get({ plain: true }));
 
-            res.render('homepage', { posts });
+            res.render('homepage', { 
+                posts,
+                
+                loggedIn: req.session.loggedIn
+            });
        })
        .catch(err => {
            console.log(err);
