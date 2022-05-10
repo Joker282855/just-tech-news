6bb851175e391478fdf40502d034b81ab5805270
unknown <jtjackson325@gmail.com>
2022-05-10 11:41:09 -0600

    got the test working for simplifying the urls address to something like fun.com

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,6 @@
 const {format_date} = require('../utils/helpers');
 const {format_plural} = require('../utils/helpers');
+const {format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
 
@@ -12,4 +13,21 @@ test('format_plural() to return a single or plural word', () => {
 
     expect(format_plural('point', 1)).toBe('point');
 
-})
+});
+
+test('format_url returns a simplified url string', () => {
+
+    const url1 = format_url('http://test.com/page/1');
+
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+
+    expect(url2).toBe('coolstuff.com');
+
+    expect(url3).toBe('google.com')
+
+});
+
