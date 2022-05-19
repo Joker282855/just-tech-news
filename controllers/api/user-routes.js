2635003bdf744a221e3063141897960d7a36f90b
unknown <jtjackson325@gmail.com>
2022-05-19 11:42:34 -0600

    fixed an accidental typo with the router function in the user-routes.js file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();ir
+const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const { User, Post, Vote, Comment } = require('../../models');
 
