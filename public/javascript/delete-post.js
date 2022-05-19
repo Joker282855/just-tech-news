06c6ea838b341b0d5e0612c6df94628bdad02f12
unknown <jtjackson325@gmail.com>
2022-05-19 14:58:13 -0600

    been double checking everything and nothing has worked for delete button so far

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,37 +1,35 @@
-async function deleteFromHandler(event) {
+async function deleteFormHandler(event) {
 
-    event.preventDefualt(event);
+    event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length-1
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/')[
+            window.location.toString().split('/').length-1
+        ]
     ];
 
-    if (title) {
+    const response = await fetch('/api/posts/:id', {
 
-        const response = await fetch('/api/posts/:id', {
+        method: 'DELETE',
 
-            method: 'DELETE',
+        body: JSON.stringify({
+            
+            post_id: id
 
-            body: JSON.stringify({
+        }),
 
-                post_id,
+        headers: {
 
-                title
+            'Content-Type': 'application/json'
 
-            }),
-
-            headers: {
-                'Content-Type': 'application/json'
-            }
-
-        });
-
-        if (response.ok) {
-            document.location.replace()
         }
-    } 
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    } else {
+        alert(response.statusText)
+    }
 
 }
 
