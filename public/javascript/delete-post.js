a3f0df4ad10b2cdf36d2046795304dcad023196c
unknown <jtjackson325@gmail.com>
2022-06-04 14:41:15 -0600

    got the delete button working on deleting the posts

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -2,42 +2,22 @@ async function deleteFormHandler(event) {
 
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/')[
-            window.location.toString().split('/').length-1
-        ]
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length-1
     ];
 
-    if (title) {
-        const response = await fetch('/api/posts/:id', {
+    const response = await fetch(`/api/posts/${id}`, {
 
-            method: 'DELETE',
-    
-            body: JSON.stringify({
-                
-                post_id,
+        method: 'DELETE'
 
-                title
-    
-            }),
-    
-            headers: {
-    
-                'Content-Type': 'application/json'
-    
-            }
+    });
 
-        });
-    
-        if (response.ok) {
-            document.location.replace('/dashboard/')
-        } else {
-            alert(response.statusText)
-        }
-    
+    if (response.ok) {
+        document.location.replace('/dashboard/')
+    } else {
+        alert(response.statusText);
     }
+
 }
 
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
